6bd0085107f88020c25f79dbb13cab3ed8214b0d
jahidulkanchan <mdj524156@gmail.com>
2024-09-23 21:23:05 +0600

    donate and history  complete

--- script/index.js
+++ script/index.js
@@ -1,14 +1,16 @@
-// Change Html file ====================
-document.getElementById('blog-btn').addEventListener('click', ()=>{
-  window.location.href = "./blog.html";
+// ======================== Donate and History =====================
+const donateBtn = document.getElementById("donate-btn");
+const historyBtn = document.getElementById("history-btn");
+const donateArea = document.getElementById("donate-area");
+const historySection = document.getElementById("history-section");
+// =================================================================
+donateBtn.addEventListener("click", ()=>{
+  toggleSections(donateBtn, historyBtn, donateArea, historySection);
+});
+historyBtn.addEventListener("click", ()=>{
+  toggleSections(historyBtn, donateBtn, historySection, donateArea);
 });
 
-// Modal Function  ==================================
-const modal = document.getElementById("modal");
-const closeModalBtn = document.getElementById("closeModalBtn");
-closeModalBtn.addEventListener("click", () =>{
-  modal.classList.add('hidden');
-})
 
 // ====================== ( Donation Place Number-1 )  =========================
 
@@ -26,20 +28,4 @@ document.getElementById("donate-btn-3").addEventListener('click', (e)=>{
   e.preventDefault();
   donateCalculation("donate-balance-3", "donate-input-3", "Aid for Injured in the Quota Movement");
 });
-
-
-
-
-// =========================================================
-  const donateBtn = document.getElementById("donate-btn");
-  const historyBtn = document.getElementById("history-btn");
-  const donateArea = document.getElementById("donate-area");
-  const historySection = document.getElementById("history-section");
-  // =================================================================
-  donateBtn.addEventListener("click", ()=>{
-    toggleSections(donateBtn, historyBtn, donateArea, historySection);
-  });
-  historyBtn.addEventListener("click", ()=>{
-    toggleSections(historyBtn, donateBtn, historySection, donateArea);
-  });
 
