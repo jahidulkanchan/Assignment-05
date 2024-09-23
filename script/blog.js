366f0323276c854e26a61611236ff2d40a4c2b0c
jahidulkanchan <mdj524156@gmail.com>
2024-09-23 21:28:52 +0600

    update common  fuction  complete

--- script/blog.js
+++ script/blog.js
@@ -2,3 +2,24 @@
 document.getElementById('home-btn').addEventListener('click', ()=>{
   window.location.href = "./index.html";
 });
+
+// Faq Items Js code ============================
+function toggleFaq(id) {
+  const content = document.getElementById(`content-${id}`);
+  const icon = document.getElementById(`icon-${id}`);
+  for (let i = 1; i <= 4; i++) {
+    if (i !== id) {
+      document.getElementById(`content-${i}`).classList.add('hidden');
+      document.getElementById(`icon-${i}`).innerHTML = `<i class="fa-solid fa-plus text-lg"></i>`;
+    }
+  }
+  if (content.classList.contains('hidden')) {
+    content.classList.remove('hidden');
+    icon.innerHTML = `<i class="fa-solid fa-minus text-lg"></i>`;
+    icon.classList.add("rotate-180");
+  } else {
+    content.classList.add('hidden');
+    icon.innerHTML = `<i class="fa-solid fa-plus text-lg"></i>`;
+    icon.classList.remove("rotate-180");
+  }
+}
